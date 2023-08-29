164b5b3644ef951534199dca5c83c305847b0887
Badal Virmani <badalvirmani1998@gmail.com>
2023-08-29 12:51:20 +0530

    Initial commit

---
+++ src/components/Header/logo.js
@@ -0,0 +1,16 @@
+
+import logo from '../../assets/investment-calculator-logo.png';
+
+const Logo = () => {
+    return (
+        <header className="header">
+        <img src={logo} alt="logo" />
+        <h1>Investment Calculator</h1>
+      </header>
+
+    )
+
+};
+
+
+export  default Logo;
